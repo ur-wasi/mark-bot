707e2c6c64ee9dac14be96bffd0b1355654eb885
ur-wasi <starkxarc@gmail.com>
2026-01-31 22:38:31 +0530

    new-version

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD Pipeline Working Successfully 🚀"
+    return "Auto Pipeline Working Successfully 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
